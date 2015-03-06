568e60d176f9efe9aef07a2e9530544db5f49178
timsk159 <timsk951@hotmail.co.uk>
2015-03-06 17:42:59 +0000

    Basic server-client connectivity

--- AdventureTownServer/Assets/Scripts/Server.cs
+++ AdventureTownServer/Assets/Scripts/Server.cs
@@ -3,14 +3,39 @@ using System.Collections;
 
 public class Server : uLink.MonoBehaviour
 {
+    public int port = 7100;
+	public int maxConnections = 64;
+    public int targetFrameRate = 60;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        Application.targetFrameRate = targetFrameRate;
+
+        uLink.Network.InitializeServer(maxConnections, port);
+    }
+
+    void uLink_OnServerInitialized()
+    {
+        Debug.Log("Server successfully started on port " + uLink.Network.listenPort);
+    }
+
+    void uLink_OnPlayerDisconnected(uLink.NetworkPlayer player)
+    {
+        uLink.Network.DestroyPlayerObjects(player);
+        uLink.Network.RemoveRPCs(player);
+
+        // this is not really necessery unless you are removing NetworkViews without calling uLink.Network.Destroy
+        uLink.Network.RemoveInstantiates(player);
+    }
+
+    void uLink_OnPlayerApproval(uLink.NetworkPlayerApproval playerApproval)
+    {
+        Debug.Log("Approving player connection");
+        playerApproval.Approve();
+    }
+
+    void uLink_OnPlayerConnected(uLink.NetworkPlayer player)
+    {
+        Debug.Log("Connection recieved from: " + player.ipAddress);
+    }
 }
